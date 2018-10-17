9072b78bf2d68d97f48336f0973b5787a434d98b
Amit Natani <eng.amitnatani1993@gmail.com>
2018-10-17 14:01:28 +0530

    Added server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var app = express();
-app.use(express.static('myApp'));
-app.get('/', function (req, res,next) {
- res.redirect('/'); 
+// var express = require('express');
+// var app = express();
+// app.use(express.static('myApp'));
+// app.get('/', function (req, res,next) {
+//  res.redirect('/'); 
+// });
+// app.listen(8080, 'localhost');
+// console.log('MyProject Server is Listening on port 8080');
+
+
+var express = require('express'),
+app = express();
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname })
 });
-app.listen(8080, 'localhost');
-console.log('MyProject Server is Listening on port 8080');
+var server = app.listen(process.env.PORT || 8080);
